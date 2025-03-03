77a502bc79e51db3118b1c89fd302b9d3270e11b
allenjamesvinoy <162484987+allenjamesvinoy@users.noreply.github.com>
2025-03-03 10:40:15 +0530

    pinger logic

---
+++ pinger.py
@@ -0,0 +1,12 @@
+import requests
+import time
+
+url = 'https://test-api-dis-docker-a88db0a1cb01.herokuapp.com/health'  # Replace with your app's URL
+
+while True:
+    try:
+        response = requests.get(url)
+        print(f'Pinged {url}, Status Code: {response.status_code}')
+    except Exception as e:
+        print(f'An error occurred: {e}')
+    time.sleep(600)  # Wait for 10 minutes before the next ping
